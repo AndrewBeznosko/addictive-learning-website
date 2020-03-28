d7cbe9920b445cf121afceee3734e5ca64071e0d
anton_vakulenchyk <anton.vakulenchyk@gmail.com>
2020-03-28 02:33:29 +0200

    slider on main page

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,4 +1,5 @@
 import TypeIt from "typeit"
+import 'slick-carousel/slick/slick';
 
 if (document.getElementById('typeit')) {
     new TypeIt('#typeit', {
@@ -14,4 +15,51 @@ if (document.getElementById('typeit')) {
     .type("Users!")
     .pause(4000)
     .go();    
+}
+
+const slider = $('#main-slider');
+
+if (slider) {
+    
+    slider.slick({
+        arrows: false,
+        speed: 350,
+        variableWidth: true,
+        touchThreshold: 20
+    });
+
+    let wheeling = false,
+        counter1 = 0, 
+        counter2;
+
+    slider.on('wheel', (function(e) {
+      e.preventDefault();
+      counter1 += 1;
+      
+      if (wheeling == false) {
+        wheeling = true;
+        wheelAct();
+        if (e.originalEvent.deltaY < 0) {
+           $(this).slick('slickPrev');
+        } else {
+            $(this).slick('slickNext');
+        }
+      }  
+    }));
+
+    function wheelAct(){
+        counter2 = counter1;
+        setTimeout(function(){
+            if (counter2 == counter1) {
+                wheelEnd();
+            } else {
+                wheelAct();
+            }
+        },30);
+    }
+    function wheelEnd(){
+        wheeling = false;
+        counter1 = 0;
+        counter2 = false;
+    }
 }
