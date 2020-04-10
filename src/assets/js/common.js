705b611fdd9f81aef7901206e574fdef94736e2d
AndrewBeznosko <andrew.beznosko@gmail.com>
2020-04-11 00:22:20 +0300

    youtube modal ready

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -2,59 +2,68 @@ import TypeIt from "typeit"
 import 'slick-carousel/slick/slick';
 import 'bootstrap-select/dist/js/bootstrap-select.min.js';
 
-// ====================================================
-// Typed text
-// ====================================================
-if (document.getElementById('typeit')) {
-    new TypeIt('#typeit', {
-            speed: 100,
-            loop: true,
-        })
-        .type("Followers!")
-        .pause(1000)
-        .delete()
-        .type("Learners!")
-        .pause(1000)
-        .delete()
-        .type("Community!")
-        .pause(4000)
-        .go();
-}
+$(document).ready(function () {
+    // ====================================================
+    // Typed text
+    // ====================================================
+    if (document.getElementById('typeit')) {
+        new TypeIt('#typeit', {
+                speed: 100,
+                loop: true,
+            })
+            .type("Followers!")
+            .pause(1000)
+            .delete()
+            .type("Learners!")
+            .pause(1000)
+            .delete()
+            .type("Community!")
+            .pause(4000)
+            .go();
+    }
 
-// ====================================================
-// Carusel
-// ====================================================
-const slider = $('#main-slider');
-const howItWorksSlider = $('#how-it-works-slider');
+    // ====================================================
+    // Carusel
+    // ====================================================
+    const slider = $('#main-slider');
+    const howItWorksSlider = $('#how-it-works-slider');
 
-if (slider) {
-    slider.slick({
-        arrows: false,
-        speed: 350,
-        variableWidth: true,
-        touchThreshold: 20,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
+    if (slider) {
+        slider.slick({
+            arrows: false,
+            speed: 350,
+            variableWidth: true,
+            touchThreshold: 20,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+        });
+    }
+
+    if (howItWorksSlider) {
+        howItWorksSlider.slick({
+            arrows: false,
+            speed: 350,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            fade: true,
+            cssEase: 'linear'
+        });
+    }
+
+    // ====================================================
+    // selectpicker
+    // ====================================================
+    const selectpicker = $('.selectpicker');
+    if (selectpicker) {
+        selectpicker.selectpicker();
+    }
+
+    // ====================================================
+    // youtube modal
+    // ====================================================
+    $(".youtube-link").grtyoutube({
+        autoPlay: true
     });
-}
-
-if (howItWorksSlider) {
-    howItWorksSlider.slick({
-        arrows: false,
-        speed: 350,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        fade: true,
-        cssEase: 'linear'
-    });
-}
-
-// ====================================================
-// selectpicker
-// ====================================================
-const selectpicker = $('.selectpicker');
-if (selectpicker) {
-    selectpicker.selectpicker();
-}
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import 'bootstrap'
 import './sass/main.scss'
+import './assets/js/grt-youtube-popup.js'
 import './assets/js/common.js'
